0d829cb9be02f1996699daaf1b4e04868fb6f2c4
liuchunsheng <1157730239@qq.com>
2025-11-04 22:06:22 +0800

    refactor: 修改http请求全局配置

--- src/main/java/com/brotherc/aquant/config/OkHttpConfig.java
+++ src/main/java/com/brotherc/aquant/config/OkHttpConfig.java
@@ -17,8 +17,8 @@ public class OkHttpConfig {
     public OkHttpClient okHttpClient() {
         return new OkHttpClient.Builder()
                 .connectTimeout(5, TimeUnit.SECONDS)
-                .readTimeout(60, TimeUnit.SECONDS)
-                .writeTimeout(60, TimeUnit.SECONDS)
+                .readTimeout(120, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
                 .connectionPool(new ConnectionPool(200, 5, TimeUnit.MINUTES))
                 .build();
     }
