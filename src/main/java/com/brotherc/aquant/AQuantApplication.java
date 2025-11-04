9b2243cd6fd29addae4cf2aa2b37bc2a0bfbdd78
liuchunsheng <1157730239@qq.com>
2025-11-04 21:52:21 +0800

    feat: 初始化项目

---
+++ src/main/java/com/brotherc/aquant/AQuantApplication.java
@@ -0,0 +1,13 @@
+package com.brotherc.aquant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AQuantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AQuantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brotherc/aquant/AQuantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherc.aquant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AQuantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
