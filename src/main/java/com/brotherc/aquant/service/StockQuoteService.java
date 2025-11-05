33c32364a9a4486639cc9d628b3a079d80eabc8c
liuchunsheng <1157730239@qq.com>
2025-11-05 20:14:03 +0800

    refactor: 优化股票行情数据同步

--- src/main/java/com/brotherc/aquant/service/StockQuoteService.java
+++ src/main/java/com/brotherc/aquant/service/StockQuoteService.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,27 +21,33 @@ public class StockQuoteService {
 
     @Transactional(rollbackFor = Exception.class)
     public void save(List<StockZhASpot> stockZhASpotList) {
-        stockQuoteRepository.deleteAll();
+        List<StockQuote> dbLlist = stockQuoteRepository.findAll();
+        Map<String, StockQuote> map = dbLlist.stream().collect(Collectors.toMap(StockQuote::getCode, o -> o));
 
-        List<StockQuote> list = stockZhASpotList.stream().map(o -> {
-            StockQuote stockQuote = new StockQuote();
-            stockQuote.setCode(o.get代码());
-            stockQuote.setName(o.get名称());
-            stockQuote.setLatestPrice(o.get最新价());
-            stockQuote.setChangeAmount(o.get涨跌额());
-            stockQuote.setChangePercent(o.get涨跌幅());
-            stockQuote.setBuyPrice(o.get买入());
-            stockQuote.setSellPrice(o.get卖出());
-            stockQuote.setPrevClose(o.get昨收());
-            stockQuote.setOpenPrice(o.get今开());
-            stockQuote.setHighPrice(o.get最高());
-            stockQuote.setLowPrice(o.get最低());
-            stockQuote.setVolume(o.get成交量());
-            stockQuote.setTurnover(o.get成交额());
-            stockQuote.setQuoteTime(o.get时间戳());
-            stockQuote.setCreatedAt(LocalDateTime.now());
-            return stockQuote;
-        }).toList();
+        List<StockQuote> list = new ArrayList<>();
+
+        for (StockZhASpot stockZhASpot : stockZhASpotList) {
+            StockQuote sq = map.get(stockZhASpot.get代码());
+            if (sq == null) {
+                sq = new StockQuote();
+            }
+            sq.setCode(stockZhASpot.get代码());
+            sq.setName(stockZhASpot.get名称());
+            sq.setLatestPrice(stockZhASpot.get最新价());
+            sq.setChangeAmount(stockZhASpot.get涨跌额());
+            sq.setChangePercent(stockZhASpot.get涨跌幅());
+            sq.setBuyPrice(stockZhASpot.get买入());
+            sq.setSellPrice(stockZhASpot.get卖出());
+            sq.setPrevClose(stockZhASpot.get昨收());
+            sq.setOpenPrice(stockZhASpot.get今开());
+            sq.setHighPrice(stockZhASpot.get最高());
+            sq.setLowPrice(stockZhASpot.get最低());
+            sq.setVolume(stockZhASpot.get成交量());
+            sq.setTurnover(stockZhASpot.get成交额());
+            sq.setQuoteTime(stockZhASpot.get时间戳());
+            sq.setCreatedAt(LocalDateTime.now());
+            list.add(sq);
+        }
         stockQuoteRepository.saveAll(list);
     }
 
