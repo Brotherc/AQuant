60d6561f3d692dd75a3b2f8fbaf9a887d7459f33
liuchunsheng <1157730239@qq.com>
2026-03-12 17:25:19 +0800

    refactor

--- aquant-backend/src/main/java/com/brotherc/aquant/exception/ExceptionEnum.java
+++ aquant-backend/src/main/java/com/brotherc/aquant/exception/ExceptionEnum.java
@@ -20,12 +20,12 @@ public enum ExceptionEnum {
     /**
      * 应用(1~2位)、服务(2位)、模块(2位)、异常(2位)
      */
-    private Integer code;
+    private final Integer code;
 
     /**
      * 异常提示信息
      */
-    private String msg;
+    private final String msg;
 
     public void throwException() {
         throw new BusinessException(this);
