1814dbe44c2c02db7c17a6fd79e23e60e88b6eca
liuchunsheng <1157730239@qq.com>
2026-08-10 20:14:03 +0800

    refactor

--- aquant-backend/src/main/java/com/brotherc/aquant/service/AKShareService.java
+++ aquant-backend/src/main/java/com/brotherc/aquant/service/AKShareService.java
@@ -61,13 +61,14 @@ public class AKShareService extends AbstractAKShareService {
     }
 
     /**
-     * 历史行情数据-新浪
-     * <p>
-     * <a href="https://finance.sina.com.cn/realstock/company/sh600006/nc.shtml">...</a>
-     * 新浪财经-沪深京 A 股的数据, 历史数据按日频率更新
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id25">历史行情数据-新浪</a>
      *
      * @param symbol 股票代码
-     * @return 历史行情日频率数据
+     * @param startDate 开始查询的日期，20201103
+     * @param endDate 结束查询的日期，20201116
+     * @param adjust 默认返回不复权的数据; qfq: 返回前复权后的数据; hfq: 返回后复权后的数据; hfq-factor: 返回后复权因子; qfq-factor: 返回前复权因子
+     *
+     * @return 历史行情数据
      */
     public List<StockZhADaily> stockZhADaily(String symbol, String startDate, String endDate, String adjust) {
         HttpUrl.Builder builder = HttpUrl.get(akshareAddress + "/api/public/stock_zh_a_daily")
@@ -88,11 +89,10 @@ public class AKShareService extends AbstractAKShareService {
     }
 
     /**
-     * 获取东方财富-业绩报表数据
-     * <p>
      * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id148">东方财富-业绩报表</a>
      *
-     * @param date 报告期，格式为 "YYYYMMDD"（如 "20231231"），通常为季度末日
+     * @param date choice of {"XXXX0331", "XXXX0630", "XXXX0930", "XXXX1231"}; 从 20100331 开始
+     *
      * @return A股上市公司业绩报表数据
      */
     public List<StockYjbbEm> stockYjbbEm(String date) {
@@ -107,6 +107,13 @@ public class AKShareService extends AbstractAKShareService {
         return stockHoldChangeCninfo("全部");
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id278">股本变动</a>
+     *
+     * @param symbol choice of {"深市主板", "沪市", "创业板", "科创板", "北交所", "全部"}
+     *
+     * @return 股本变动
+     */
     public List<StockHoldChangeCninfo> stockHoldChangeCninfo(String symbol) {
         HttpUrl.Builder builder = HttpUrl.get(akshareAddress + "/api/public/stock_hold_change_cninfo")
                 .newBuilder();
@@ -118,6 +125,13 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(builder.build(), new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id168">分红配送-东财</a>
+     *
+     * @param date choice of {"XXXX0630", "XXXX1231"}; 从 19901231 开始
+     *
+     * @return 分红配送
+     */
     public List<StockFhpsEm> stockFhpsEm(String date) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/stock_fhps_em")
                 .newBuilder()
@@ -126,6 +140,13 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id169">分红配送详情-东财</a>
+     *
+     * @param symbol 股票代码
+     *
+     * @return 分红配送详情
+     */
     public List<StockFhpsDetailEm> stockFhpsDetailEm(String symbol) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/stock_fhps_detail_em")
                 .newBuilder()
@@ -134,6 +155,13 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id377">分红情况-同花顺</a>
+     *
+     * @param symbol 股票代码
+     *
+     * @return 分红情况
+     */
     public List<StockFhpsDetailThs> stockFhpsDetailThs(String symbol) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/stock_fhps_detail_ths")
                 .newBuilder()
@@ -142,10 +170,24 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id377">同花顺-同花顺行业一览表</a>
+     *
+     * @return 当前时刻同花顺行业一览表
+     */
     public List<StockBoardIndustrySummaryThs> stockBoardIndustrySummaryThs() {
         return executeGet(akshareAddress + "/api/public/stock_board_industry_summary_ths", new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id378">同花顺-指数</a>
+     *
+     * @param symbol 行业名称
+     * @param startDate 开始时间，20200101
+     * @param endDate 结束时间 20211027
+     *
+     * @return 板块日频指数数据
+     */
     public List<StockBoardIndustryIndexThs> stockBoardIndustryIndexThs(String symbol, String startDate, String endDate) {
         HttpUrl.Builder builder = HttpUrl.get(akshareAddress + "/api/public/stock_board_industry_index_ths")
                 .newBuilder()
@@ -161,6 +203,11 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(builder.build(), new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/fund/fund_public.html#id1">基金基本信息</a>
+     *
+     * @return 基金基本信息
+     */
     public List<FundNameEm> fundNameEm() {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/fund_name_em")
                 .newBuilder()
@@ -168,6 +215,15 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/fund/fund_public.html#id17">开放式基金-历史数据</a>
+     *
+     * @param symbol 基金代码
+     * @param indicator choice of {"单位净值走势", "累计净值走势", "累计收益率走势", "同类排名走势", "同类排名百分比", "分红送配详情", "拆分详情"}
+     * @param period choice of {"1月", "3月", "6月", "1年", "3年", "5年", "今年来", "成立来"}，"成立来"该参数只对 累计收益率走势 有效
+     *
+     * @return 基金历史数据
+     */
     public List<FundOpenFundInfoEm> fundOpenFundInfoEm(String symbol, String indicator, String period) {
         HttpUrl.Builder builder = HttpUrl.get(akshareAddress + "/api/public/fund_open_fund_info_em")
                 .newBuilder();
@@ -187,6 +243,11 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(builder.build(), new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/fund/fund_public.html#id5">基金申购状态</a>
+     *
+     * @return 基金申购列表
+     */
     public List<FundPurchaseEm> fundPurchaseEm() {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/fund_purchase_em")
                 .newBuilder()
@@ -194,6 +255,14 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/fund/fund_public.html#id46">基金持仓</a>
+     *
+     * @param symbol 基金代码，symbol="000001"
+     * @param date 指定年份, date="2024"；传入空字符串 "" 时返回最新可用年份数据
+     *
+     * @return 基金持仓列表
+     */
     public List<FundPortfolioHoldEm> fundPortfolioHoldEm(String symbol, String date) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/fund_portfolio_hold_em")
                 .newBuilder()
@@ -203,6 +272,13 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id258">终止/暂停上市-深证</a>
+     *
+     * @param symbol choice of {"暂停上市公司", "终止上市公司"}
+     *
+     * @return 深证交易所终止/暂停上市股票代码列表
+     */
     public List<StockInfoSzDelist> stockInfoSzDelist(String symbol) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/stock_info_sz_delist")
                 .newBuilder()
@@ -211,6 +287,13 @@ public class AKShareService extends AbstractAKShareService {
         return executeGet(httpUrl, new TypeReference<>() {});
     }
 
+    /**
+     * <a href="https://akshare.akfamily.xyz/data/stock/stock.html#id258">暂停/终止上市-上证</a>
+     *
+     * @param symbol choice of {"全部", "沪市", "科创板"}
+     *
+     * @return 上证交易所暂停/终止上市股票代码列表
+     */
     public List<StockInfoShDelist> stockInfoShDelist(String symbol) {
         HttpUrl httpUrl = HttpUrl.get(akshareAddress + "/api/public/stock_info_sh_delist")
                 .newBuilder()
