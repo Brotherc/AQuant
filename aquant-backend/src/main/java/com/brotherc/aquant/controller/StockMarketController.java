4cb88185aaf25b1975ec83d8221a234bd0dff4ba
liuchunsheng <1157730239@qq.com>
2026-08-11 14:03:11 +0800

    refactor

--- aquant-backend/src/main/java/com/brotherc/aquant/controller/StockMarketController.java
+++ aquant-backend/src/main/java/com/brotherc/aquant/controller/StockMarketController.java
@@ -4,8 +4,7 @@ import com.brotherc.aquant.model.dto.common.ResponseDTO;
 import com.brotherc.aquant.model.vo.fundflow.FundFlowGraphVO;
 import com.brotherc.aquant.model.vo.fundflow.FundFlowSummaryVO;
 import com.brotherc.aquant.model.vo.sentiment.MarketSentimentVO;
-import com.brotherc.aquant.service.FundFlowService;
-import com.brotherc.aquant.service.MarketSentimentService;
+import com.brotherc.aquant.service.index.StockMarketService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,25 +20,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stockMarket")
 public class StockMarketController {
 
-    private final MarketSentimentService marketSentimentService;
-    private final FundFlowService fundFlowService;
+    private final StockMarketService stockMarketService;
 
     @Operation(summary = "查询基于本地股票实时表统计的市场情绪与温度计")
     @GetMapping("/current")
     public ResponseDTO<MarketSentimentVO> getMarketSentiment() {
-        return ResponseDTO.success(marketSentimentService.getMarketSentiment());
+        return ResponseDTO.success(stockMarketService.getMarketSentiment());
     }
 
     @Operation(summary = "查询资金流动网络关系图数据")
     @GetMapping("/fundFlow/graph")
     public ResponseDTO<FundFlowGraphVO> getGraphData() {
-        return ResponseDTO.success(fundFlowService.getGraphData());
+        return ResponseDTO.success(stockMarketService.getGraphData());
     }
 
     @Operation(summary = "查询大盘资金流动汇总数据")
     @GetMapping("/fundFlow/summary")
     public ResponseDTO<FundFlowSummaryVO> getSummaryData() {
-        return ResponseDTO.success(fundFlowService.getSummaryData());
+        return ResponseDTO.success(stockMarketService.getSummaryData());
     }
 
 }
